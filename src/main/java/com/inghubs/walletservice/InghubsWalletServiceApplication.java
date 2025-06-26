1bd56c1e719cb89422871bf50cc65503c5bc5c1a
Koray Güney <koray.guney@hotmail.com>
2025-06-26 20:42:35 +0400

    Initial commit

---
+++ src/main/java/com/inghubs/walletservice/InghubsWalletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inghubs.walletservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InghubsWalletServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InghubsWalletServiceApplication.class, args);
+    }
+
+}
